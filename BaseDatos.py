8d4fe2a61ff9128f5cf61a65d9e7ffdf691eb878
German Escobar <germanjosearielescobarescobar@gmail.com>
2025-08-21 23:00:56 -0600

    Primer push

---
+++ BaseDatos.py
@@ -0,0 +1,26 @@
+class Producto:
+    pass
+
+class Ventas:
+    pass
+
+class Compras:
+    pass
+
+class Empleados:
+    pass
+
+class Inventario:
+    pass
+
+class Busqueda:
+    pass
+
+class Listar:
+    pass
+
+class Menus:
+    pass
+
+class Clientes:
+    pass
