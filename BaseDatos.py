b1ba36c85d64adfde00254a825ab7fdf9167ace2
German Escobar <germanjosearielescobarescobar@gmail.com>
2025-08-22 21:11:31 -0600

    Primer push

--- BaseDatos.py
+++ BaseDatos.py
@@ -1,5 +1,17 @@
 class Producto:
-    pass
+    def __init__(self, id_producto, nombre, precio, id_categoria, total_compras=0, total_ventas=0, stock=0):
+        self.id_producto = id_producto
+        self.nombre = nombre
+        self.precio = precio
+        self.id_categoria = id_categoria
+        self.total_compras = total_compras
+        self.total_ventas = total_ventas
+        self.stock = stock
+
+class Categoria:
+    def __init__(self, id_categoria, nombre):
+        self.id_categoria = id_categoria
+        self.nombre = nombre
 
 class Ventas:
     pass
