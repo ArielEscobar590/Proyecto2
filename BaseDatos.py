646219159ebaed321120f9721de2f8bf3f62f584
German Escobar <germanjosearielescobarescobar@gmail.com>
2025-08-24 07:10:18 -0600

    Primer push

--- BaseDatos.py
+++ BaseDatos.py
@@ -1,14 +1,30 @@
-class Producto:
-    def __init__(self, id_producto, nombre, precio, id_categoria, total_compras=0, total_ventas=0, stock=0):
-        self.id_producto = id_producto
+class Personas:
+    def __init__(self, nombre, direccion, telefono, correo):
         self.nombre = nombre
+        self.direccion = direccion
+        self.telefono = telefono
+        self.correo = correo
+
+class Empleados(Personas):
+    pass
+
+class Clientes(Personas):
+    pass
+
+class Proveedores(Personas):
+    pass
+
+class Motores:
+    def __init__(self, id_codigo, num_motor, precio, cilindrada, total_compras=0, total_ventas=0, stock=0):
+        self.id_codigo = id_codigo
+        self.num_motor = num_motor
         self.precio = precio
-        self.id_categoria = id_categoria
+        self.cilindrada = cilindrada
         self.total_compras = total_compras
         self.total_ventas = total_ventas
         self.stock = stock
 
-class Categoria:
+class Categorias:
     def __init__(self, id_categoria, nombre):
         self.id_categoria = id_categoria
         self.nombre = nombre
@@ -16,11 +32,16 @@ class Categoria:
 class Ventas:
     pass
 
+class DetalleVentas:
+    pass
+
 class Compras:
     pass
 
-class Empleados:
+class DetalleCompras:
     pass
+
+
 
 class Inventario:
     pass
@@ -31,8 +52,13 @@ class Busqueda:
 class Listar:
     pass
 
-class Menus:
+class Modificar:
     pass
 
-class Clientes:
-    pass
+class Menus:
+    def MenuPrincipal(self):
+        print("--Menu principal---")
+
+    def MenuPrincipal2(self):
+        print("--Menu principal2---")
+
